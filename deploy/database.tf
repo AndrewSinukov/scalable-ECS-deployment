677d9d4c4e683300ce56086b7cd7659108e40a7c
AndrewSinukov <andrew.sinukov.it@gmail.com>
2022-01-03 14:30:25 +0200

    setup ecs

--- deploy/database.tf
+++ deploy/database.tf
@@ -20,6 +20,11 @@ resource "aws_security_group" "rds" {
     protocol  = "tcp"
     from_port = 5432
     to_port   = 5432
+
+    security_groups = [
+      aws_security_group.bastion.id,
+      aws_security_group.ecs_service.id
+    ]
   }
 
   tags = local.common_tags
--- deploy/variables.tf
+++ deploy/variables.tf
@@ -22,3 +22,18 @@ variable "bastion_key_name" {
   default = "app-api-devops-bastion"
 }
 
+variable "ecr_image_api" {
+  description = "ECR Image for API"
+  default     = "<APP ECR Image URL>:latest"
+}
+
+variable "ecr_image_proxy" {
+  description = "ECR Image for API"
+  default     = "<App ECR Image for Proxy>:latest"
+}
+
+variable "django_secret_key" {
+  description = "Secret key for Django app"
+}
+
+
